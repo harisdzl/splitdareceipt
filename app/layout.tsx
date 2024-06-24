d8adb25d711061cbc11f30d016d745b99cc1d413
Haris Dzulkifli <97676887+harisdzl@users.noreply.github.com>
2024-06-24 10:07:11 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Analytics />
         <Navbar />
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
